f2cc121c61e40334256ad616cb71e0a3f9a34ece
krebstock <kyle.rebstock@eleadcrm.com>
2018-05-25 09:31:29 -0500

    format model.ts

--- quickstart/src/app/model.ts
+++ quickstart/src/app/model.ts
@@ -1,12 +1,12 @@
-export class Customer  {
-    id: number;
-    name: string;
-    address: Address;
+export class Customer {
+  id: number;
+  name: string;
+  address: Address;
 }
 
 export class Address {
-    city: string;
-    street: string;
-    state: string;
-    region: string;
+  street: string;
+  city: string;
+  state: string;
+  region: string;
 }
