8b136d12c136fbf388b55dc293a66b6120ad0c83
krebstock <kyle.rebstock@eleadcrm.com>
2018-05-25 09:27:45 -0500

    format document customer-detial.component.ts

--- quickstart/src/app/customer-detail.component.ts
+++ quickstart/src/app/customer-detail.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Customer } from './model';
 
+
 @Component({
   moduleId: module.id,
-  selector: 'customer-detail', // to use me, use as html tag <my-app>
+  selector: 'customer-detail',
   templateUrl: 'customer-detail.component.html'
 })
 
-export class CustomerDetailComponent  {
-  @Input() customer: Customer;
+export class CustomerDetailComponent {
+  @Input()
+  customer: Customer;
+
   showAddress = true;
 }
