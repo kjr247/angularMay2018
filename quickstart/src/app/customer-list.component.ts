f243c3a808850b646fc3a8218190d2270c993442
krebstock <kyle.rebstock@eleadcrm.com>
2018-05-25 09:30:49 -0500

    insanity formatting property alignment for why?

--- quickstart/src/app/customer-list.component.ts
+++ quickstart/src/app/customer-list.component.ts
@@ -15,43 +15,43 @@ export class CustomerListComponent  {
 
   customers: Customer[] = [
     {
-      id: 1,
-      name: 'Alex Smith',
+      id:       1,
+      name:    'Alex Smith',
       address: {
-        street: '123 Main Street',
-        city: 'Anytown',
-        state: 'California',
-        region: 'West'
+        street:  '123 Main Street',
+        city:    'Anytown',
+        state:   'California',
+        region:  'West'
       }
     },
     {
-      id: 2,
-      name: 'Pierre Pasmal',
+      id:       2,
+      name:    'Pierre Pasmal',
       address: {
-        street: '456 Rue de Main',
-        city: 'Quebec City',
-        state: 'Quebec',
-        region: 'East'
+        street:  '456 Rue de Main',
+        city:    'Quebec City',
+        state:   'Quebec',
+        region:  'East'
       }
     },
     {
-      id: 3,
-      name: 'Margarita Nadie',
+      id:       3,
+      name:    'Margarita Nadie',
       address: {
-        street: '789 Calle Principal',
-        city: 'Guadalajara',
-        state: 'Jalisco',
-        region: 'South'
+        street:  '789 Calle Principal',
+        city:    'Guadalajara',
+        state:   'Jalisco',
+        region:  'South'
       }
     },
     {
-      id: 4,
-      name: 'Katie O\'Leary',
+      id:       4,
+      name:    'Katie O\'Leary',
       address: {
-        street: '137 DeKoven Street',
-        city: 'Chicago',
-        state: 'Illinois',
-        region: 'Midwest'
+        street:  '137 DeKoven Street',
+        city:    'Chicago',
+        state:   'Illinois',
+        region:  'Midwest'
       }
     },
   ];
