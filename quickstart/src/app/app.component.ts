cd6e5ceda4c15ee7070ddd9397bfcf2733d6387c
krebstock <kyle.rebstock@eleadcrm.com>
2018-05-25 09:21:48 -0500

    cleaup

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
+
 @Component({
-  moduleId: module.id,
-  selector: 'my-app', // to use me, use as html tag <my-app>
+  selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <customer-list></customer-list>
-  `
+    <customer-list></customer-list>`
 })
-export class AppComponent {
-  title = 'Customer Management';
-}
+export class AppComponent  { title = 'Customer Management'; }
